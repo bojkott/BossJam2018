4a5bb93a4e585186461fa560ee543983c07af610
Morgan Cromell <morgan.cromell@hotmail.com>
2018-05-12 00:18:24 +0200

    knife kill!

---
+++ Assets/bossjam/Scripts/HandFollowTarget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandFollowTarget : MonoBehaviour {
+
+
+    new protected Rigidbody rigidbody;
+
+    public float force = 10;
+    public float minDiff = 0;
+    public float leadTime = 0.3f;
+    public Transform target = null;
+    //
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+
+        Vector3 diff = target.position - transform.position + rigidbody.velocity*leadTime;
+        Debug.Log(diff);
+        if(diff.magnitude > minDiff)
+        {
+            rigidbody.AddForce(diff * force * Time.deltaTime, ForceMode.Impulse);
+        }
+
+    }
+}
