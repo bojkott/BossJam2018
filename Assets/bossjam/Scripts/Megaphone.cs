1a5742fb847bb720870668dced43b95c4307efaa
Oskar Warnhag <oskar.warnhag@gmail.com>
2018-05-12 15:17:45 +0200

    Almost got sweaty attack working

---
+++ Assets/bossjam/Scripts/Megaphone.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Megaphone : MonoBehaviour {
+
+    public float damage = 0.1f;
+    Resource resource;
+    public ParticleSystem particles;
+    public LayerMask canAttack;
+    bool cooldown = false;
+    float cooldownTimer = 0.5f;
+	// Use this for initialization
+	void Start () {
+        resource = GetComponentInParent<Resource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ResetCooldown()
+    {
+        cooldown = false;
+    }
+    void StartCooldown()
+    {
+        cooldown = true;
+        Invoke("ResetCooldown", cooldownTimer);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!cooldown && ((1 << collision.gameObject.layer) & canAttack) != 0)
+        {
+            //It matched one
+            Resource otherResource = collision.gameObject.GetComponentInParent<Resource>();
+            if (!otherResource.IsDead())
+            {
+                otherResource.RemovePoints(damage);
+                resource.AddPoints(damage);
+                particles.Play();
+                StartCooldown();
+            }
+            
+
+        }
+    }
+}
